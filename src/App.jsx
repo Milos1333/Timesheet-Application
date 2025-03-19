6b1d7f1689d26673ebda09a843ff3e1914615af9
Milos1333 <milosklepic91@gmail.com>
2025-03-20 00:20:33 +0100

    html-css-structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="page-wrap">
+      {/* header */}
+      <header className="header">
+        <div className="wrap">
+          <span className="btn-icon">
+            <img
+              className="icon icon-plus js-modal-init"
+              src="icons/icon-plus.svg"
+              alt="Add New Item"
+            />
+          </span>
+          <div className="header-blockquote">
+            <h1 className="header-quote">
+              Happiness is good health and a bad memory
+            </h1>
+            <div className="header-cite">- Ingrid Bergman</div>
+          </div>
+        </div>
+        <div className="header-inner">
+          <div className="wrap">
+            <img className="logo" src="images/vegait-logo.svg" alt="VegaIT" />
+            <div className="date-wrap">
+              <img
+                className="icon"
+                src="icons/icon-calendar.svg"
+                alt="Calendar"
+              />
+              <time>02 / 08 / 2019</time>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="main">
+        <div className="wrap">
+          {/* item */}
+          <div className="item-row">
+            <div className="check-flag">
+              <span className="small-text-label">Title</span>
+              <span className="small-text-label hours">Hours</span>
+              <span className="check-flag-label">
+                Ticket system integration
+              </span>
+              <span className="hours-box" />
+            </div>
+          </div>
+          {/* item */}
+          <div className="item-row">
+            <div className="check-flag">
+              <span className="small-text-label">Title</span>
+              <span className="small-text-label hours">Hours</span>
+              <span className="check-flag-label">
+                Integration with Google Maps API
+              </span>
+              <span className="hours-box" />
+            </div>
+          </div>
+          {/* item */}
+          <div className="item-row">
+            <div className="check-flag">
+              <span className="small-text-label">Title</span>
+              <span className="small-text-label hours">Hours</span>
+              <span className="check-flag-label">Prepare test cases</span>
+              <span className="hours-box" />
+            </div>
+          </div>
+          <div className="total align-right">
+            <label htmlFor className="total-label">
+              Total:
+            </label>
+            <input className="total-input" type="text" defaultValue readOnly />
+          </div>
+        </div>
+      </main>
+      {/* modal window */}
+      <div className="modal-wrap js-modal">
+        <div className="modal js-modal-inner">
+          <h2>Create a task:</h2>
+          <form action>
+            <div className="field-wrap">
+              <label className="label" htmlFor>
+                Title:
+              </label>
+              <input
+                className="field"
+                type="text"
+                id
+                placeholder="Enter title here..."
+              />
+            </div>
+            <div className="field-wrap">
+              <label className="label" htmlFor>
+                Hours:
+              </label>
+              <input
+                className="field"
+                type="text"
+                id
+                placeholder="Add hours here..."
+              />
+            </div>
+            <div className="btn-wrap align-right">
+              <input className="btn" type="submit" defaultValue="Create" />
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {/* footer */}
+      <footer className="footer">
+        <div className="wrap">
+          <span className="copy">© 2019 Vega IT Sourcing</span>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default App
+export default App;
